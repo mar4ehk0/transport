413063618f8053e6e7f0f90d3842c201a428d5be
mar4ehk0 <mar4ehk0@gmail.com>
2022-09-10 16:01:27 +0300

    Adds AppValidator.php

---
+++ app/src/AppValidator.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Mar4ehk0;
+
+class AppValidator
+{
+
+    public function __construct(private array $options)
+    {
+    }
+
+    public function validate(): bool
+    {
+        if (empty($this->options) || empty($this->options['f'])) {
+            throw new \InvalidArgumentException('Please give me file. index.php -f filename.csv');
+        }
+
+        if (file_exists($this->options['f'])) {
+            return true;
+        }
+
+        throw new \InvalidArgumentException('Please check path to file.');
+    }
+}
